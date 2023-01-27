d0f6418f41695171ccc8f1fa3d71f726b8a059b6
0xmostafam <mostafamohamed8881@gmail.com>
2023-01-27 14:53:11 +0200

    Feat: Added roles and registration

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +47,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof UnauthorizedException) {
+            return response()->json([
+                'message' => 'You do not have the required authorization.',
+                'status' => 403,
+            ]);
+        }
+
+        return parent::render($request, $exception);
     }
 }
