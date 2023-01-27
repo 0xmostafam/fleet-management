7ab01ed94a9abbf0f0d7a2155859c7ebb6d97279
0xmostafam <mostafamohamed8881@gmail.com>
2023-01-27 14:03:25 +0200

    Feat: Finished main user reservation endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,9 @@ Route::middleware('auth:sanctum')->group(function () {
     // User Routes
     Route::post('/logout', [AuthController::class, 'logout']);
 
+    // Seats routes group
+    Route::prefix('/reservations')->group(function () {
+        Route::get('/start/{start_station}/end/{end_station}', [ReservationController::class, 'index']);
+        Route::post('book', [ReservationController::class, 'book']);
+    });
 });
